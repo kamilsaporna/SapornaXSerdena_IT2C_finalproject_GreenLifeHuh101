f9175fb75a7a6b60bbfcc1da670820593f9ba11c
AngelicaSerdena <angelicaserdena770@gmail.com>
2023-06-03 23:50:18 +0800

    Fifth commit

---
+++ blog-post.php
@@ -0,0 +1,356 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>SustainableLiving101 </title>
+	<!-- Bootstrap core CSS -->
+	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Fontawesome CSS -->
+	<link href="css/all.css" rel="stylesheet">
+	<!-- Custom styles for this template -->
+	<link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+    <!-- Navigation -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-light top-nav fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+            <img src="images_project\LOGOFINAL.png" alt="logo" />
+            </a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="fas fa-bars"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+               <ul class="navbar-nav ml-auto">
+                  <li class="nav-item">
+                     <a class="nav-link" href="index.php">Home</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="about.php">About</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="services.php">Services</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                     Portfolio
+                     </a>
+                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+                        <a class="dropdown-item" href="portfolio-1-col.php">Green Living Tips</a>
+                        <a class="dropdown-item" href="portfolio-2-col.php">Gardening Urban Farming</a>
+                        <a class="dropdown-item" href="portfolio-3-col.php">DIY Projects</a>
+                        <a class="dropdown-item" href="portfolio-4-col.php">Community and Engagement</a>
+                        <a class="dropdown-item" href="portfolio-item.php">Environmental News Education</a>
+                   </div>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Blog
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+                       <a class="dropdown-item" href="blog-home-1.php">Sustainable Home</a>
+                       <a class="dropdown-item" href="blog-home-2.php">Green Technology</a>
+                       <a class="dropdown-item" href="blog-post.php">Eco-Friendly Gardening</a>
+                    </div>
+                 </li>
+                  <li class="nav-item dropdown">
+                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                     Pages
+                     </a>
+                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+                        <a class="dropdown-item" href="faq.php">FAQ</a>
+                        <a class="dropdown-item" href="404.php">404</a>
+                        <a class="dropdown-item" href="pricing.php">Pricing Table</a>
+                     </div>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="contact.php">Contact</a>
+                  </li>
+               </ul>
+            </div>
+        </div>
+    </nav>
+	
+	<!-- full Title -->
+	<div class="full-title">
+		<div class="container">
+			<!-- Page Heading/Breadcrumbs -->
+			<h1 class="mt-4 mb-3">Eco-Friendly Gardening
+				<small>Harmonizing Nature and Nurture: Embrace Eco-Friendly Gardening Practices for a Sustainable Landscape</small>
+			</h1>
+		</div>
+	</div>
+
+    <!-- Page Content -->
+    <div class="container">
+		<div class="breadcrumb-main">
+			<ol class="breadcrumb">
+				<li class="breadcrumb-item">
+					<a href="index.php">Home</a>
+				</li>
+				<li class="breadcrumb-item active">Eco-Friendly Gardening</li>
+			</ol>
+		</div>
+
+		<div class="row">
+        <!-- Post Content Column -->
+			<div class="col-lg-8">
+
+				<!-- Preview Image -->
+				<img class="img-fluid rounded" src="images_project\eco_gardening.jpg" alt="" />
+				<hr>
+					<!-- Date/Time -->
+					<p>Posted on January 1, 2022 at 18:00 PM</p>
+				<hr>
+				<!-- Post Content -->
+				<p class="lead"> Organic Gardening</p>
+
+				<p>Embrace organic gardening practices by avoiding synthetic fertilizers, pesticides, and herbicides. Instead, utilize natural alternatives like compost, mulch, and organic pest control methods to maintain the health of your plants and soil. Organic gardening promotes soil fertility, protects beneficial insects, and reduces chemical runoff.</p>
+
+
+				<blockquote class="blockquote">
+					<p class="mb-0">Water Conservation</p>
+					<footer class="blockquote-footer">Author
+						<cite title="Source Title">William Ashworth</cite>
+					</footer>
+				</blockquote>
+
+				<p>Practice water conservation techniques to minimize water waste and promote efficient irrigation. Install a rainwater harvesting system to collect and store rainwater for watering your garden. Use drip irrigation or soaker hoses to deliver water directly to the plant roots, reducing evaporation and water runoff. Mulching around plants helps retain moisture and suppress weed growth.</p>
+
+		
+
+			<hr>
+
+			
+			<div class="blog-right-side">
+				<!-- Comments Form -->
+				<div class="card my-4">
+					<h5 class="card-header">Leave a Comment:</h5>
+					<div class="card-body">
+						<form>
+							<div class="form-group">
+								<textarea class="form-control" rows="3"></textarea>
+							</div>
+							<button type="submit" class="btn btn-primary">Submit</button>
+						</form>
+					</div>
+				</div>
+			</div>
+
+          <!-- Single Comment -->
+          <div class="media mb-4">
+            <img class="d-flex mr-3 rounded-circle" src="images_project\commenter1.png" alt="">
+            <div class="media-body">
+              <h5 class="mt-0">Kamiru Familara</h5>
+              Kudos to your team for creating such a user-friendly and visually appealing website. Navigating through the content is a breeze, and the design truly reflects the essence of sustainability!!
+            </div>
+          </div>
+
+          <!-- Comment with nested comments -->
+          <div class="media mb-4">
+            <img class="d-flex mr-3 rounded-circle" src="images_project\commenter2.png" alt="">
+            <div class="media-body">
+              <h5 class="mt-0">Gabriel Magculang</h5>
+              I just wanted to say how much I appreciate the insightful articles on your website. The content is well-researched, engaging, and offers practical tips for incorporating sustainable practices into everyday life.
+
+              <div class="media mt-4">
+                <img class="d-flex mr-3 rounded-circle" src="images_project\commenter2.png" alt="">
+                <div class="media-body">
+                  <h5 class="mt-0">Gabriel Magculang</h5>
+                  I can't thank you enough for the valuable resources and guides available on your website. The information provided has helped me make positive changes towards a more sustainable lifestyle. Keep up the excellent work!
+                </div>
+              </div>
+
+              <div class="media mt-4">
+                <img class="d-flex mr-3 rounded-circle" src="images_project\commenter3.png" alt="">
+                <div class="media-body">
+                  <h5 class="mt-0">Nash Bartolome</h5>
+                  Your website is a wealth of inspiration for anyone looking to make a difference in the world through sustainable choices. The stories and success stories shared are incredibly motivating and remind us that every action counts.
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+
+        <!-- Sidebar Widgets Column -->
+        <div class="col-md-4 blog-right-side">
+
+          <!-- Search Widget -->
+          <div class="card mb-4">
+            <h5 class="card-header">Search</h5>
+            <div class="card-body">
+              <div class="input-group">
+                <input type="text" class="form-control" placeholder="Search for...">
+                <span class="input-group-btn">
+                  <button class="btn btn-secondary" type="button">Go!</button>
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <!-- Categories Widget -->
+          <div class="card my-4">
+            <h5 class="card-header">Categories</h5>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-lg-6">
+                  <ul class="list-unstyled mb-0">
+                    <li>
+                      <a href="#">Integrated Pest Management (IPM)</a>
+                    </li>
+                    <li>
+                      <a href="#">Sustainable Garden Design</a>
+                    </li>
+                    <li>
+                      <a href="#">Companion Planting</a>
+                    </li>
+                  </ul>
+                </div>
+                <div class="col-lg-6">
+                  <ul class="list-unstyled mb-0">
+                    <li>
+                      <a href="#">Composting and Recycling</a>
+                    </li>
+                    <li>
+                      <a href="#">Native Plants</a>
+                    </li>
+                    <li>
+                      <a href="#">Water Conservation</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- Side Widget -->
+          <div class="card my-4">
+            <h5 class="card-header">Message</h5>
+            <div class="card-body">
+              Embrace the beauty of nature while nurturing the planet. Eco-friendly gardening is more than just growing plants; it's a sustainable way to connect with the Earth. By adopting organic practices, conserving water, and supporting biodiversity, we can create thriving gardens that not only enhance our surroundings but also contribute to a healthier and greener world. Let's cultivate a harmonious relationship with nature and grow gardens that sustain both our souls and the environment.
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+    <!--footer starts from here-->
+    <footer class="footer">
+        <div class="container bottom_border">
+            <div class="row">
+               <div class="col-lg-3 col-md-6 col-sm-6 col">
+					<h5 class="headin5_amrc col_white_amrc pt2">Find us</h5>
+					<!--headin5_amrc-->
+					<p class="mb10"> We are committed to building a strong and engaged community of individuals passionate about sustainable living. Wherever you choose to connect with us, we look forward to sharing valuable information, inspiration, and support on your sustainable living journey.
+						Here are the various ways to find and connect with us:
+					</p>
+					<p><i class="fa fa-location-arrow"></i> 4905 P4 Brgy. Cabugao </p>
+					<p><i class="fa fa-phone"></i> +63-966-422-6295 or +63-997-782-1751</p>
+					<p><i class="fa fa fa-envelope"></i> kamilsaporna123@gmail.com or Angelicaserdena770@gmail.com</p>
+
+               </div>
+               <div class="col-lg-3 col-md-6 col-sm-6 col">
+					<h5 class="headin5_amrc col_white_amrc pt2">Follow us</h5>
+					<!--headin5_amrc ends here-->
+					<ul class="footer_ul2_amrc">
+            <li>
+							<a href="#"><i class="fab fa-facebook fleft padding-right"></i> </a>
+							<p>FACEBOOK <a href="#">https://www.facebook.com
+								/min.chaeeun.58</a></p>
+						</li>
+						<li>
+							<a href="#"><i class="fab fa-facebook fleft padding-right"></i> </a>
+							<p>FACEBOOK <a href="#">https://www.facebook.com
+								/acilegna.anedres</a></p>
+						</li>
+						<li>
+							<a href="#"><i class="fab fa-instagram fleft padding-right"></i> </a>
+							<p>INSTAGRAM <a href="#">https://www.instagram.com
+								/kamirusenpai/</a></p>
+						</li>
+
+					</ul>
+					<!--footer_ul2_amrc ends here-->
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<h5 class="headin5_amrc col_white_amrc pt2">Quick links</h5>
+					<!--headin5_amrc-->
+					<ul class="footer_ul_amrc">
+						<li><a href="#">Default Version</a></li>
+						<li><a href="#">Boxed Version</a></li>
+						<li><a href="#">Our Team </a></li>
+						<li><a href="#">About Us</a></li>
+						<li><a href="#">Our Services</a></li>
+						<li><a href="#">Get In Touch</a></li>
+					</ul>
+					<!--footer_ul_amrc ends here-->
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6 ">
+					<h5 class="headin5_amrc col_white_amrc pt2">Recent posts</h5>
+					<!--headin5_amrc-->
+					<ul class="footer_ul_amrc">
+						<li class="media">
+							<div class="media-left">
+								<img class="img-fluid" src="images/post-img-01.jpg" alt="" />
+							</div>
+							<div class="media-body">
+								<p>How to find best dog food?</p>
+								<span>22 Sep 2018</span>
+							</div>
+						</li>
+						<li class="media">
+							<div class="media-left">
+								<img class="img-fluid" src="images/post-img-02.jpg" alt="" />
+							</div>
+							<div class="media-body">
+								<p>How to find best dog food?</p>
+								<span>34 Sep 2018</span>
+							</div>
+						</li>
+						<li class="media">
+							<div class="media-left">
+								<img class="img-fluid" src="images/post-img-03.jpg" alt="" />
+							</div>
+							<div class="media-body">
+								<p>How to find best dog food?</p>
+								<span>30 Sep 2018</span>
+							</div>
+						</li>
+					</ul>
+					<!--footer_ul_amrc ends here-->
+				</div>
+			</div>
+		</div>
+        <div class="container">
+            <div class="footer-logo">
+				<a href="#"><img src="images_project\LOGOFINAL.png" alt="" /></a>
+			</div>
+            <!--foote_bottom_ul_amrc ends here-->
+            <p class="copyright text-center">All Rights Reserved. &copy; 2018 <a href="#">GreenLifeHub101</a> Design By : 
+				<a href="https://html.design/">html design</a>
+            </p>
+            <ul class="social_footer_ul">
+				<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+				<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+				<li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+				<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+            <!--social_footer_ul ends here-->
+        </div>
+    </footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
