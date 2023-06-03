a62f9fdaa9381c6b4be731fbceca781694e1c557
AngelicaSerdena <angelicaserdena770@gmail.com>
2023-06-03 23:58:00 +0800

    Thirteen commit

---
+++ portfolio-4-col.php
@@ -0,0 +1,327 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>GreenLifeHub101</title>
+	<!-- Bootstrap core CSS -->
+	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Fontawesome CSS -->
+	<link href="css/all.css" rel="stylesheet">
+	<!-- Custom styles for this template -->
+	<link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+    <!-- Navigation -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-light top-nav fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+            <img src="images_project\LOGOFINAL.png" alt="logo" />
+            </a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="fas fa-bars"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+               <ul class="navbar-nav ml-auto">
+                  <li class="nav-item">
+                     <a class="nav-link" href="index.php">Home</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="about.php">About</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="services.php">Services</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                     <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                     Portfolio
+                     </a>
+                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+                        <a class="dropdown-item" href="portfolio-1-col.php">Green Living Tips</a>
+                        <a class="dropdown-item" href="portfolio-2-col.php">Gardening Urban Farming</a>
+                        <a class="dropdown-item" href="portfolio-3-col.php">DIY Projects</a>
+                        <a class="dropdown-item" href="portfolio-4-col.php">Community and Engagement</a>
+                        <a class="dropdown-item" href="portfolio-item.php">Environmental News Education</a>
+                   </div>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Blog
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+                       <a class="dropdown-item" href="blog-home-1.php">Sustainable Home</a>
+                       <a class="dropdown-item" href="blog-home-2.php">Green Technology</a>
+                       <a class="dropdown-item" href="blog-post.php">Eco-Friendly Gardening</a>
+                    </div>
+                 </li>
+                  
+                  <li class="nav-item dropdown">
+                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                     Pages
+                     </a>
+                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+                        <a class="dropdown-item" href="faq.php">FAQ</a>
+                        <a class="dropdown-item" href="404.php">404</a>
+                        <a class="dropdown-item" href="pricing.php">Pricing Table</a>
+                     </div>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="contact.php">Contact</a>
+                  </li>
+               </ul>
+            </div>
+        </div>
+    </nav>
+	
+	<!-- full Title -->
+	<div class="full-title">
+		<div class="container">
+			<!-- Page Heading/Breadcrumbs -->
+			<h1 class="mt-4 mb-3">Community and Engagement
+				<small>Connect, Collaborate, and Create Change: Engage with Like-Minded Individuals in the Sustainable Living Community</small>
+			</h1>
+		</div>
+	</div>
+
+    <!-- Page Content -->
+    <div class="container">
+		<div class="breadcrumb-main">
+			<ol class="breadcrumb">
+				<li class="breadcrumb-item">
+					<a href="index.php">Home</a>
+				</li>
+				<li class="breadcrumb-item active">Community and Engagement</li>
+			</ol>
+		</div>
+
+      <div class="row">
+        <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a ="#"><img class="card-img-top" src="images_project\COMM. 0.webp" alt="" /></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Introduction to Community and Engagement</a>
+              </h4>
+              <p class="card-text">Building a community of like-minded individuals is crucial for promoting sustainable living and making a positive impact. This section of your website focuses on fostering engagement, connecting people, and providing a platform for sharing ideas and experiences related to sustainable living>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="images_project\COMM. 2.jpg" alt="" /></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Volunteers opportunities</a>
+              </h4>
+              <p class="card-text">Promote local volunteer opportunities that align with sustainable living values. This can include environmental cleanup events, community garden projects, conservation efforts, or tree planting initiatives. Provide information on how visitors can get involved, including contact details or registration links for volunteering.>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="images_project\COMM. 5.jpg" alt="" /></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Sustainable Living Challenges</a>
+              </h4>
+              <p class="card-text">Organize periodic sustainable living challenges that encourage visitors to adopt eco-friendly practices in their daily lives. These challenges can focus on reducing waste, conserving energy and water, practicing mindful consumption, or adopting sustainable transportation options. Provide guidelines, resources, and a platform for participants to share their progress and experiences.>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="images_project\COMM. 4.jpg" alt="" /></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Community Spotlights and Success Stories</a>
+              </h4>
+              <p class="card-text">Highlight individuals or organizations within the community who are making a significant impact in sustainable living. Share success stories, case studies, or interviews that inspire others and showcase real-life examples of sustainable practices. This can include profiles of local eco-entrepreneurs, sustainable initiatives by schools or businesses, or stories of individuals implementing sustainable changes in their homes and communities.>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="images_project\community2.jpg" alt="" /></a>
+            <div class="card-body">04.jp
+              <h4 class="card-title">
+                <a href="#">Interactive Polls and Surveys</a>
+              </h4>
+              <p class="card-text">Conduct polls and surveys to gather insights and opinions from your website visitors. These can cover a range of sustainability-related topics, such as preferences for eco-friendly products, attitudes towards recycling, or awareness of environmental issues. Use the collected data to create informative content, tailor your offerings, and spark further discussions.>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="images_project\COMM. 6.jpg" alt="" /></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">User-Generated Content</a>
+              </h4>
+              <p class="card-text">Allow visitors to contribute their own content, such as articles, blog posts, or personal stories, related to sustainable living. Create a submission process and guidelines for user-generated content to ensure quality and relevance. This not only encourages engagement but also provides a platform for individuals to share their experiences and knowledge with a wider audience>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="images_project\community.jpg" alt="" /></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Sustainable Living Challenges</a>
+              </h4>
+              <p class="card-text">Organize periodic sustainable living challenges that encourage visitors to adopt eco-friendly practices in their daily lives. These challenges can focus on reducing waste, conserving energy and water, practicing mindful consumption, or adopting sustainable transportation options. Provide guidelines, resources, and a platform for participants to share their progress and experiences>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="images_project\COMM. 3.avif" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Social Media Integration</a>
+              </h4>
+              <p class="card-text">Integrate your website with social media platforms to extend community engagement beyond the website itself. Encourage visitors to follow your social media accounts, participate in discussions, and share your content with their networks. This expands your reach, fosters connections, and encourages ongoing engagement with your sustainability community.>
+            </div>
+          </div>
+        </div>
+      </div>
+	  
+		<div class="pagination_bar">
+			<!-- Pagination -->
+			<ul class="pagination justify-content-center">
+				<li class="page-item">
+				  <a class="page-link" href="#" aria-label="Previous">
+					<span aria-hidden="true">&laquo;</span>
+					<span class="sr-only">Previous</span>
+				  </a>
+				</li>
+				<li class="page-item">
+				  <a class="page-link" href="#">1</a>
+				</li>
+				<li class="page-item">
+				  <a class="page-link" href="#">2</a>
+				</li>
+				<li class="page-item">
+				  <a class="page-link" href="#">3</a>
+				</li>
+				<li class="page-item">
+				  <a class="page-link" href="#" aria-label="Next">
+					<span aria-hidden="true">&raquo;</span>
+					<span class="sr-only">Next</span>
+				  </a>
+				</li>
+			</ul>
+		</div>
+
+    </div>
+    <!-- /.container -->
+
+    <!--footer starts from here-->
+    <footer class="footer">
+        <div class="container bottom_border">
+            <div class="row">
+               <div class="col-lg-3 col-md-6 col-sm-6 col">
+					<h5 class="headin5_amrc col_white_amrc pt2">Find us</h5>
+					<!--headin5_amrc-->
+					<p class="mb10"> We are committed to building a strong and engaged community of individuals passionate about sustainable living. Wherever you choose to connect with us, we look forward to sharing valuable information, inspiration, and support on your sustainable living journey.
+						Here are the various ways to find and connect with us:
+					</p>
+					<p><i class="fa fa-location-arrow"></i> 4905 P4 Brgy. Cabugao </p>
+					<p><i class="fa fa-phone"></i> +63-966-422-6295 or +63-997-782-1751</p>
+					<p><i class="fa fa fa-envelope"></i> kamilsaporna123@gmail.com or Angelicaserdena770@gmail.com</p>
+               </div>
+               <div class="col-lg-3 col-md-6 col-sm-6 col">
+					<h5 class="headin5_amrc col_white_amrc pt2">Follow us</h5>
+					<!--headin5_amrc ends here-->
+					<ul class="footer_ul2_amrc">
+            <li>
+							<a href="#"><i class="fab fa-facebook fleft padding-right"></i> </a>
+							<p>FACEBOOK <a href="#">https://www.facebook.com
+								/min.chaeeun.58</a></p>
+						</li>
+						<li>
+							<a href="#"><i class="fab fa-facebook fleft padding-right"></i> </a>
+							<p>FACEBOOK <a href="#">https://www.facebook.com
+								/acilegna.anedres</a></p>
+						</li>
+						<li>
+							<a href="#"><i class="fab fa-instagram fleft padding-right"></i> </a>
+							<p>INSTAGRAM <a href="#">https://www.instagram.com
+								/kamirusenpai/</a></p>
+						</li>
+					</ul>
+					<!--footer_ul2_amrc ends here-->
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<h5 class="headin5_amrc col_white_amrc pt2">Quick links</h5>
+					<!--headin5_amrc-->
+					<ul class="footer_ul_amrc">
+						<li><a href="#">Default Version</a></li>
+						<li><a href="#">Boxed Version</a></li>
+						<li><a href="#">Our Team </a></li>
+						<li><a href="#">About Us</a></li>
+						<li><a href="#">Our Services</a></li>
+						<li><a href="#">Get In Touch</a></li>
+					</ul>
+					<!--footer_ul_amrc ends here-->
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6 ">
+					<h5 class="headin5_amrc col_white_amrc pt2">Recent posts</h5>
+					<!--headin5_amrc-->
+					<ul class="footer_ul_amrc">
+						<li class="media">
+							<div class="media-left">
+								<img class="img-fluid" src="images/post-img-01.jpg" alt="" />
+							</div>
+							<div class="media-body">
+								<p>How to find best dog food?</p>
+								<span>22 Sep 2018</span>
+							</div>
+						</li>
+						<li class="media">
+							<div class="media-left">
+								<img class="img-fluid" src="images/post-img-02.jpg" alt="" />
+							</div>
+							<div class="media-body">
+								<p>How to find best dog food?</p>
+								<span>34 Sep 2018</span>
+							</div>
+						</li>
+						<li class="media">
+							<div class="media-left">
+								<img class="img-fluid" src="images/post-img-03.jpg" alt="" />
+							</div>
+							<div class="media-body">
+								<p>How to find best dog food?</p>
+								<span>30 Sep 2018</span>
+							</div>
+						</li>
+					</ul>
+					<!--footer_ul_amrc ends here-->
+				</div>
+			</div>
+		</div>
+        <div class="container">
+            <div class="footer-logo">
+				<a href="#"><img src="images_project\LOGOFINAL.png" alt="" /></a>
+			</div>
+            <!--foote_bottom_ul_amrc ends here-->
+            <p class="copyright text-center">All Rights Reserved. &copy; 2018 <a href="#">GreenLifeHub101</a> Design By : 
+				<a href="https://html.design/">html design</a>
+            </p>
+            <ul class="social_footer_ul">
+				<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+				<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+				<li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+				<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+            <!--social_footer_ul ends here-->
+        </div>
+    </footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
