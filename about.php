25ed91502c5af11bac9d6ecc90e3271f46ab5c5a
AngelicaSerdena <angelicaserdena770@gmail.com>
2023-06-03 23:44:07 +0800

    Second commit

---
+++ about.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>GreenLifeHub101 </title>
+	<!-- Bootstrap core CSS -->
+	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Fontawesome CSS -->
+	<link href="css/all.css" rel="stylesheet">
+	<!-- Custom styles for this template -->
+	<link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+	<!-- Navigation -->
+	
+
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-light top-nav fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+            <img src="images_project\LOGOFINAL.png" alt="logo" />
+            </a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="fas fa-bars"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+               <ul class="navbar-nav ml-auto">
+                  <li class="nav-item">
+                     <a class="nav-link" href="index.php">Home</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link active" href="about.php">About</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                     Portfolio
+                     </a>
+					 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+                        <a class="dropdown-item" href="portfolio-1-col.php">Green Living Tips</a>
+                        <a class="dropdown-item" href="portfolio-2-col.php">Gardening Urban Farming</a>
+                        <a class="dropdown-item" href="portfolio-3-col.php">DIY Projects</a>
+                        <a class="dropdown-item" href="portfolio-4-col.php">Community and Engagement</a>
+                        <a class="dropdown-item" href="portfolio-item.php">Environmental News Education</a>
+                     </div>
+                  </li>
+				  <li class="nav-item dropdown">
+					<a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Blog
+					</a>
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+					   <a class="dropdown-item" href="blog-home-1.php">Sustainable Home</a>
+					   <a class="dropdown-item" href="blog-home-2.php">Green Technology</a>
+					   <a class="dropdown-item" href="blog-post.php">Eco-Friendly Gardening</a>
+					</div>
+				 </li>
+                  <li class="nav-item dropdown">
+                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                     Pages
+                     </a>
+                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+                        <a class="dropdown-item" href="faq.php">FAQ</a>
+                        <a class="dropdown-item" href="404.php">404</a>
+                        <a class="dropdown-item" href="pricing.php">Pricing Table</a>
+                     </div>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="contact.php">Contact</a>
+                  </li>
+               </ul>
+            </div>
+        </div>
+    </nav>
+	<!-- full Title -->
+	<div class="full-title">
+		<div class="container">
+			<!-- Page Heading/Breadcrumbs -->
+			<h1 class="mt-4 mb-3">About
+				<small>Learn about Us</small>
+			</h1>
+		</div>
+	</div>
+    <!-- Page Content -->
+    <div class="container">
+		<div class="breadcrumb-main">
+			<ol class="breadcrumb">
+				<li class="breadcrumb-item">
+					<a href="index.php">Home</a>
+				</li>
+				<li class="breadcrumb-item active">About</li>
+			</ol>
+		</div>
+		<!-- About Content -->
+		<div class="about-main">
+			<div class="row">
+				<div class="col-lg-6">
+					<img class="img-fluid rounded mb-4" src="images_project\goodimage1.jpg" alt="" />
+				</div>
+				<div class="col-lg-6">
+					<h2>About Us</h2>
+					<p>At GreenLifeHub101, we are dedicated to promoting and facilitating sustainable living practices for a greener and more environmentally conscious world. Our mission is to empower individuals and communities to adopt eco-friendly lifestyles, make informed choices, and contribute to a sustainable future.</p>
+					<p>Through our platform, we strive to provide a comprehensive resource that educates, inspires, and supports individuals on their sustainable living journey. We believe that every small action counts and that collective efforts can create a significant positive impact on the planet.</p>
+					<p>Join us in our commitment to create a sustainable and thriving planet. Explore our website, engage with our community, and discover the resources and inspiration you need to embrace a more eco-conscious lifestyle. Together, we can make a difference and shape a better world for generations to come.</p>
+				</div>
+			</div>
+			<!-- /.row -->
+		</div>
+
+		<!-- Team Members -->
+		<div class="team-members-box">  
+			<h2>Our Team</h2>
+			<div class="row">
+				<div class="col-lg-4 mb-4">
+				  <div class="card h-100 text-center">
+					<img class="card-img-top" src="images_project\kamil.jpeg" alt="" />
+					<div class="card-body">
+						<h4 class="card-title">Kamil Saporna</h4>
+						<h6 class="card-subtitle mb-2 text-muted">Manager</h6>
+						<p class="card-text">As the Manager of GreenLifeHub101, I play a crucial role in leading our sustainable living platform towards success. My primary responsibilities will include overseeing the day-to-day operations, ensuring smooth functioning of the website, and fostering a thriving community of users passionate about sustainable living.
+						</p>
+					</div>
+					<div class="card-footer">
+						<ul>
+							<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+							<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+						</ul>
+					</div>
+				  </div>
+				</div>
+				<div class="col-lg-4 mb-4">
+				  <div class="card h-100 text-center">
+					<img class="card-img-top" src="images_project\angelica.jpg" alt="" />
+					<div class="card-body">
+						<h4 class="card-title">Angelica Serdena</h4>
+						<h6 class="card-subtitle mb-2 text-muted">Assistant Manager</h6>
+						<p class="card-text">As the Assistant Manager of GreenLifeHub101, I play a pivotal role in supporting the Manager and assisting with the day-to-day operations of our sustainable living platform. Our main responsibilities will include overseeing content production, managing community engagement, and ensuring the smooth functioning of the website.</p>
+					</div>
+					<div class="card-footer">
+						<ul>
+							<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+							<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+						</ul>
+					</div>
+				  </div>
+				</div>
+				<div class="col-lg-4 mb-4">
+				  <div class="card h-100 text-center">
+					<img class="card-img-top" src="images_project\mikeru.JPG" alt="" />
+					<div class="card-body">
+						<h4 class="card-title">Michael Permejo</h4>
+						<h6 class="card-subtitle mb-2 text-muted">Social Media Manager</h6>
+						<p class="card-text">If you are a creative and driven individual with a genuine passion for sustainability, join us as the Social Media Manager of GreenLifeHub101. Together, we can leverage the power of social media to inspire and empower others to embrace a more sustainable lifestyle..</p>
+					</div>
+					<div class="card-footer">
+						<ul>
+							<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+							<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+						</ul>
+					</div>
+				  </div>
+				</div>
+			</div>
+			<!-- /.row -->
+		</div>
+		<!-- Our Customers -->
+		<div class="customers-box"> 
+			<h2>Our Customers</h2>
+			<div class="row">
+				<div class="col-lg-2 col-sm-4 mb-4">
+				  <img class="img-fluid" src="images\logo_01.png" alt="" />
+				</div>
+				<div class="col-lg-2 col-sm-4 mb-4">
+				  <img class="img-fluid" src="images/logo_02.png" alt="" />
+				</div>
+				<div class="col-lg-2 col-sm-4 mb-4">
+				  <img class="img-fluid" src="images/logo_03.png" alt="" />
+				</div>
+				<div class="col-lg-2 col-sm-4 mb-4">
+				  <img class="img-fluid" src="images/logo_04.png" alt="" />
+				</div>
+				<div class="col-lg-2 col-sm-4 mb-4">
+				  <img class="img-fluid" src="images/logo_05.png" alt="" />
+				</div>
+				<div class="col-lg-2 col-sm-4 mb-4">
+				  <img class="img-fluid" src="images/logo_06.png" alt="" />
+				</div>
+			</div>
+			<!-- /.row -->
+		</div>
+		<!-- /.container -->
+	</div>
+
+    <!--footer starts from here-->
+    <footer class="footer">
+        <div class="container bottom_border">
+            <div class="row">
+               <div class="col-lg-3 col-md-6 col-sm-6 col">
+					<h5 class="headin5_amrc col_white_amrc pt2">Find us</h5>
+					<!--headin5_amrc-->
+					<p class="mb10"> We are committed to building a strong and engaged community of individuals passionate about sustainable living. Wherever you choose to connect with us, we look forward to sharing valuable information, inspiration, and support on your sustainable living journey.
+						Here are the various ways to find and connect with us:
+					</p>
+					<p><i class="fa fa-location-arrow"></i> 4905 P4 Brgy. Cabugao </p>
+					<p><i class="fa fa-phone"></i> +63-966-422-6295 or +63-997-782-1751</p>
+					<p><i class="fa fa fa-envelope"></i> kamilsaporna123@gmail.com or Angelicaserdena770@gmail.com</p>
+               </div>
+               <div class="col-lg-3 col-md-6 col-sm-6 col">
+					<h5 class="headin5_amrc col_white_amrc pt2">Follow us</h5>
+					<!--headin5_amrc ends here-->
+					<ul class="footer_ul2_amrc">
+						<li>
+							<a href="#"><i class="fab fa-facebook fleft padding-right"></i> </a>
+							<p>FACEBOOK <a href="#">https://www.facebook.com
+								/min.chaeeun.58</a></p>
+						</li>
+						<li>
+							<a href="#"><i class="fab fa-facebook fleft padding-right"></i> </a>
+							<p>FACEBOOK <a href="#">https://www.facebook.com
+								/acilegna.anedres</a></p>
+						</li>
+						<li>
+							<a href="#"><i class="fab fa-instagram fleft padding-right"></i> </a>
+							<p>INSTAGRAM <a href="#">https://www.instagram.com
+								/kamirusenpai/</a></p>
+						</li>
+
+					</ul>
+					<!--footer_ul2_amrc ends here-->
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<h5 class="headin5_amrc col_white_amrc pt2">Quick links</h5>
+					<!--headin5_amrc-->
+					<ul class="footer_ul_amrc">
+						<li><a href="#">Default Version</a></li>
+						<li><a href="#">Boxed Version</a></li>
+						<li><a href="#">Our Team </a></li>
+						<li><a href="#">About Us</a></li>
+						<li><a href="#">Our Services</a></li>
+						<li><a href="#">Get In Touch</a></li>
+					</ul>
+					<!--footer_ul_amrc ends here-->
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6 ">
+					<h5 class="headin5_amrc col_white_amrc pt2">Recent posts</h5>
+					<!--headin5_amrc-->
+					<ul class="footer_ul_amrc">
+						<li class="media">
+							<div class="media-left">
+								<img class="img-fluid" src="images_project\LOGOFINAL.png" alt="" />
+							</div>
+							<div class="media-body">
+								<p>How to find best dog food?</p>
+								<span>22 Sep 2018</span>
+							</div>
+						</li>
+						<li class="media">
+							<div class="media-left">
+								<img class="img-fluid" src="images/post-img-02.jpg" alt="" />
+							</div>
+							<div class="media-body">
+								<p>How to find best dog food?</p>
+								<span>34 Sep 2018</span>
+							</div>
+						</li>
+						<li class="media">
+							<div class="media-left">
+								<img class="img-fluid" src="images/post-img-03.jpg" alt="" />
+							</div>
+							<div class="media-body">
+								<p>How to find best dog food?</p>
+								<span>30 Sep 2018</span>
+							</div>
+						</li>
+					</ul>
+					<!--footer_ul_amrc ends here-->
+				</div>
+			</div>
+		</div>
+        <div class="container">
+            <div class="footer-logo">
+				<a href="#"><img src="images_project\LOGOFINAL.png" alt="" /></a>
+			</div>
+            <!--foote_bottom_ul_amrc ends here-->
+            <p class="copyright text-center">All Rights Reserved. &copy; 2018 <a href="#">GreenHub101</a> Design By : 
+				<a href="https://html.design/">html design</a>
+            </p>
+            <ul class="social_footer_ul">
+				<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+				<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+				<li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+				<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+            <!--social_footer_ul ends here-->
+        </div>
+    </footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
