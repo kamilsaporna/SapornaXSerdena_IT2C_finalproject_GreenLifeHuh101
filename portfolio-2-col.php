f5f42fc932c475cda06c3966fa955001e77753b9
AngelicaSerdena <angelicaserdena770@gmail.com>
2023-06-03 23:55:47 +0800

    Eleventh commit

---
+++ portfolio-2-col.php
@@ -0,0 +1,325 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>GreenLifeHub101</title>
+	<!-- Bootstrap core CSS -->
+	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Fontawesome CSS -->
+	<link href="css/all.css" rel="stylesheet">
+	<!-- Custom styles for this template -->
+	<link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+    <!-- Navigation -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-light top-nav fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+            <img src="images_project\LOGOFINAL.png" alt="logo" />
+            </a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="fas fa-bars"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+               <ul class="navbar-nav ml-auto">
+                  <li class="nav-item">
+                     <a class="nav-link" href="index.php">Home</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="about.php">About</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="services.php">Services</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                     <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                     Portfolio
+                     </a>
+                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+                        <a class="dropdown-item" href="portfolio-1-col.php">Green Living Tips</a>
+                        <a class="dropdown-item" href="portfolio-2-col.php">Gardening Urban Farming</a>
+                        <a class="dropdown-item" href="portfolio-3-col.php">DIY Projects</a>
+                        <a class="dropdown-item" href="portfolio-4-col.php">Community and Engagement</a>
+                        <a class="dropdown-item" href="portfolio-item.php">Environmental News Education</a>
+                   </div>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Blog
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+                       <a class="dropdown-item" href="blog-home-1.php">Sustainable Home</a>
+                       <a class="dropdown-item" href="blog-home-2php">Green Technology</a>
+                       <a class="dropdown-item" href="blog-post.php">Eco-Friendly Gardening</a>
+                    </div>
+                 </li>
+                  <li class="nav-item dropdown">
+                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                     Pages
+                     </a>
+                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+                        <a class="dropdown-item" href="faq.php">FAQ</a>
+                        <a class="dropdown-item" href="404.php">404</a>
+                        <a class="dropdown-item" ="pricing.php">Pricing Table</a>
+                     </div>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" ="contact.php">Contact</a>
+                  </li>
+               </ul>
+            </div>
+        </div>
+    </nav>
+	
+	<!-- full Title -->
+	<div class="full-title">
+		<div class="container">
+			<!-- Page Heading/Breadcrumbs -->
+			<h1 class="mt-4 mb-3"> Gardening and Urban Farming
+				<small>From City to Garden: Unlock the Joys of Gardening and Urban Farming for Sustainable Living</small>
+			</h1>
+		</div>
+	</div>
+
+    <!-- Page Content -->
+    <div class="container">
+		<div class="breadcrumb-main">
+			<ol class="breadcrumb">
+				<li class="breadcrumb-item">
+					<a href="index.php">Home</a>
+				</li>
+				<li class="breadcrumb-item active">Gardening and Urban Farming</li>
+			</ol>
+	  </div>
+
+      <div class="row">
+        <div class="col-lg-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="images_project\eco_gardening.jpg" alt="" /></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Benefits of Gardening and Urban Farming</a>
+              </h4>
+              <p class="card-text">Gardening and urban farming offer numerous benefits for individuals and communities alike. They provide access to fresh, organic produce, which promotes a healthy and sustainable lifestyle. By growing food locally, it reduces the carbon footprint associated with long-distance transportation. Gardening and urban farming also contribute to food security and self-sufficiency, as individuals can rely on their own harvests. Moreover, engaging in gardening activities promotes physical activity, mental well-being, and stress reduction. Lastly, these practices create green spaces in urban areas, beautifying the surroundings and supporting biodiversity.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="images_project\eco_gardening2.jpg" alt="" /></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">To get started with gardening, consider the following steps:</a>
+              </h4>
+              <p class="card-text">Assess the available space, whether it's a backyard, balcony, or community garden plot.
+                Determine the amount of sunlight the space receives to choose appropriate plants.
+                Check the quality of the soil and amend it if needed by adding compost or organic matter.
+                Select plants suitable for your local climate and gardening experience level.
+                Start with easy-to-grow herbs, vegetables, or flowers to build confidence.
+                Use sustainable gardening practices, such as using organic fertilizers, composting, and practicing natural pest control methods.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="images_project\eco_gardening4.jpg" alt="" /></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Urban farming</a>
+              </h4>
+              <p class="card-text">Utilizing rooftop gardens, balconies, or vertical gardening systems.
+                Exploring hydroponics or aquaponics, which allow for soil-less cultivation.
+                Employing container gardening or raised bed gardening techniques.
+                Incorporating composting to enrich the soil and reduce organic waste.
+                Considering keeping backyard chickens or engaging in urban beekeeping for additional sustainability.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="images_project\SHAPE TREE.jpg" alt="" /></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Sustainable Gardening Techniques</a>
+              </h4>
+              <p class="card-text">Sharing resources, knowledge, and experiences among gardeners.
+                Establishing shared gardening spaces to maximize productivity and diversity.
+                Organizing workshops, classes, or workdays to educate and involve the community.
+                Collaborating with local schools, organizations, or businesses to promote urban farming initiatives.
+                Creating opportunities for social interaction and fostering a sense of belonging.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="images_project\showeringplants.jpg" alt="" /></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Resources and Tools</a>
+              </h4>
+              <p class="card-text">Recommendations for gardening books, websites, and online forums.
+                Tips for sourcing organic seeds, seedlings, and gardening supplies.
+                Guides on composting, soil testing, and plant care techniques.
+                Information on local gardening events, workshops, and educational programs.
+                Highlighting relevant gardening tools and equipment for sustainable practices.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="images_project\gardening.jpg" alt="" /></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Organic Pest Control Methods</a>
+              </h4>
+              <p class="card-text">Share organic pest control methods to help gardeners manage pests without resorting to chemical pesticides.
+                Provide information on natural pest deterrents like companion planting, trap crops, and physical barriers.
+                Discuss the use of beneficial insects, such as ladybugs and praying mantises, to control pest populations.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /.row -->
+
+		<div class="pagination_bar">
+			<!-- Pagination -->
+			<ul class="pagination justify-content-center">
+				<li class="page-item">
+				  <a class="page-link" href="#" aria-label="Previous">
+					<span aria-hidden="true">&laquo;</span>
+					<span class="sr-only">Previous</span>
+				  </a>
+				</li>
+				<li class="page-item">
+				  <a class="page-link" href="#">1</a>
+				</li>
+				<li class="page-item">
+				  <a class="page-link" href="#">2</a>
+				</li>
+				<li class="page-item">
+				  <a class="page-link" href="#">3</a>
+				</li>
+				<li class="page-item">
+				  <a class="page-link" href="#" aria-label="Next">
+					<span aria-hidden="true">&raquo;</span>
+					<span class="sr-only">Next</span>
+				  </a>
+				</li>
+			</ul>
+		</div>
+    </div>
+    <!-- /.container -->
+
+    <!--footer starts from here-->
+    <footer class="footer">
+        <div class="container bottom_border">
+            <div class="row">
+               <div class="col-lg-3 col-md-6 col-sm-6 col">
+					<h5 class="headin5_amrc col_white_amrc pt2">Find us</h5>
+					<!--headin5_amrc-->
+          <p class="mb10"> We are committed to building a strong and engaged community of individuals passionate about sustainable living. Wherever you choose to connect with us, we look forward to sharing valuable information, inspiration, and support on your sustainable living journey.
+						Here are the various ways to find and connect with us:
+					</p>
+					<p><i class="fa fa-location-arrow"></i> 4905 P4 Brgy. Cabugao </p>
+					<p><i class="fa fa-phone"></i> +63-966-422-6295 or +63-997-782-1751</p>
+					<p><i class="fa fa fa-envelope"></i> kamilsaporna123@gmail.com or Angelicaserdena770@gmail.com</p>
+
+               </div>
+               <div class="col-lg-3 col-md-6 col-sm-6 col">
+					<h5 class="headin5_amrc col_white_amrc pt2">Follow us</h5>
+					<!--headin5_amrc ends here-->
+					<ul class="footer_ul2_amrc">
+						<li>
+							<a href="#"><i class="fab fa-facebook fleft padding-right"></i> </a>
+							<p>FACEBOOK <a href="#">https://www.facebook.com
+								/min.chaeeun.58</a></p>
+						</li>
+						<li>
+							<a href="#"><i class="fab fa-facebook fleft padding-right"></i> </a>
+							<p>FACEBOOK <a href="#">https://www.facebook.com
+								/acilegna.anedres</a></p>
+						</li>
+						<li>
+							<a href="#"><i class="fab fa-instagram fleft padding-right"></i> </a>
+							<p>INSTAGRAM <a href="#">https://www.instagram.com
+								/kamirusenpai/</a></p>
+						</li>
+
+					</ul>
+					<!--footer_ul2_amrc ends here-->
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<h5 class="headin5_amrc col_white_amrc pt2">Quick links</h5>
+					<!--headin5_amrc-->
+					<ul class="footer_ul_amrc">
+						<li><a href="#">Default Version</a></li>
+						<li><a href="#">Boxed Version</a></li>
+						<li><a href="#">Our Team </a></li>
+						<li><a href="#">About Us</a></li>
+						<li><a href="#">Our Services</a></li>
+						<li><a href="#">Get In Touch</a></li>
+					</ul>
+					<!--footer_ul_amrc ends here-->
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6 ">
+					<h5 class="headin5_amrc col_white_amrc pt2">Recent posts</h5>
+					<!--headin5_amrc-->
+					<ul class="footer_ul_amrc">
+						<li class="media">
+							<div class="media-left">
+								<img class="img-fluid" src="images/post-img-01.jpg" alt="" />
+							</div>
+							<div class="media-body">
+								<p>How to find best dog food?</p>
+								<span>22 Sep 2018</span>
+							</div>
+						</li>
+						<li class="media">
+							<div class="media-left">
+								<img class="img-fluid" src="images/post-img-02.jpg" alt="" />
+							</div>
+							<div class="media-body">
+								<p>How to find best dog food?</p>
+								<span>34 Sep 2018</span>
+							</div>
+						</li>
+						<li class="media">
+							<div class="media-left">
+								<img class="img-fluid" src="images/post-img-03.jpg" alt="" />
+							</div>
+							<div class="media-body">
+								<p>How to find best dog food?</p>
+								<span>30 Sep 2018</span>
+							</div>
+						</li>
+					</ul>
+					<!--footer_ul_amrc ends here-->
+				</div>
+			</div>
+		</div>
+        <div class="container">
+            <div class="footer-logo">
+				<a href="#"><img src="images_project\LOGOFINAL.png" alt="" /></a>
+			</div>
+            <!--foote_bottom_ul_amrc ends here-->
+            <p class="copyright text-center">All Rights Reserved. &copy; 2018 <a href="#">GreenLifeHub101</a> Design By : 
+				<a href="https://html.design/">html design</a>
+            </p>
+            <ul class="social_footer_ul">
+				<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+				<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+				<li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+				<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+            <!--social_footer_ul ends here-->
+        </div>
+    </footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
