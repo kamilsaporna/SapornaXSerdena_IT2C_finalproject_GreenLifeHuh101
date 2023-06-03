8953881903792f2e02fbcc97f3410224636a9370
AngelicaSerdena <angelicaserdena770@gmail.com>
2023-06-03 23:49:16 +0800

    Fourth commit

---
+++ blog-home-2.php
@@ -0,0 +1,323 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>GreenLifeHub101 </title>
+	<!-- Bootstrap core CSS -->
+	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Fontawesome CSS -->
+	<link href="css/all.css" rel="stylesheet">
+	<!-- Custom styles for this template -->
+	<link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+    <!-- Navigation -->
+
+
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-light top-nav fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="index.html">
+            <img src="images_project\LOGOFINAL.png" alt="logo" />
+            </a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="fas fa-bars"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+               <ul class="navbar-nav ml-auto">
+                  <li class="nav-item">
+                     <a class="nav-link" href="index.php">Home</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="about.php">About</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" ="services.php">Services</a>
+                  </li>
+				  <li class="nav-item dropdown">
+                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                     Portfolio
+                     </a>
+					 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+                        <a class="dropdown-item" href="portfolio-1-col.php">Green Living Tips</a>
+                        <a class="dropdown-item" href="portfolio-2-col.php">Gardening Urban Farming</a>
+                        <a class="dropdown-item" href="portfolio-3-col.php">DIY Projects</a>
+                        <a class="dropdown-item" href="portfolio-4-col.php">Community and Engagement</a>
+                        <a class="dropdown-item" href="portfolio-item.php">Environmental News Education</a>
+                     </div>
+                  </li>
+				  <li class="nav-item dropdown">
+					<a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Blog
+					</a>
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+					   <a class="dropdown-item" href="blog-home-1.php">Sustainable Home</a>
+					   <a class="dropdown-item" href="blog-home-2.php">Green Technology</a>
+					   <a class="dropdown-item" href="blog-post.php">Eco-Friendly Gardening</a>
+					</div>
+				 </li>
+                  <li class="nav-item dropdown">
+                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                     Pages
+                     </a>
+                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+                        <a class="dropdown-item" href="faq.php">FAQ</a>
+                        <a class="dropdown-item" href="404.php">404</a>
+                        <a class="dropdown-item" href="pricing.php">Pricing Table</a>
+                     </div>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="contact.php">Contact</a>
+                  </li>
+               </ul>
+            </div>
+        </div>
+    </nav>
+	
+	<!-- full Title -->
+	<div class="full-title">
+		<div class="container">
+			<!-- Page Heading/Breadcrumbs -->
+			<h1 class="mt-4 mb-3">Green Technology 
+				<small>Welcome to the "Green Technology" section, where we explore innovative solutions and practices that promote sustainable living in residential settings. Our goal is to empower homeowners with the knowledge and tools to create environmentally friendly and energy-efficient homes, reducing their carbon footprint and contributing to a greener future.
+
+				</small>
+			</h1>
+		</div>
+	</div>
+
+    <!-- Page Content -->
+    <div class="container">
+		<div class="breadcrumb-main">
+			<ol class="breadcrumb">
+				<li class="breadcrumb-item">
+					<a href="index.php">Home</a>
+				</li>
+				<li class="breadcrumb-item active">Green Technology</li>
+			</ol>
+		</div>
+
+		<div class="blog-entries">
+			<!-- Blog Post -->
+			<div class="card mb-4">
+				<div class="card-body">
+				  <div class="row">
+					<div class="col-lg-6">
+					  <a href="#">
+						<img class="img-fluid rounded" src="images_project\greentech.jpg" alt="" />
+					  </a>
+					</div>
+					<div class="col-lg-6">
+					  <h2 class="card-title">Introduction to green technology for homes</h2>
+					  <p class="card-text">Green technology for homes focuses on incorporating sustainable practices and technologies to reduce environmental impact and improve energy efficiency. It encompasses various solutions that promote a greener lifestyle and contribute to a more sustainable future. By adopting green technology in homes, individuals can minimize their carbon footprint, conserve resources, and create a healthier living environment.
+
+					  </p>
+					  <a href="#" class="btn btn-primary">Read More &rarr;</a>
+					</div>
+				  </div>
+				</div>
+				<div class="card-footer text-muted">
+				  Posted on January 1, 2017 by
+				  <a href="#">GreenLifeHub101</a>
+				</div>
+			</div>
+
+			<!-- Blog Post -->
+			<div class="card mb-4">
+				<div class="card-body">
+				  <div class="row">
+					<div class="col-lg-6">
+					  <a href="#">
+						<img class="img-fluid rounded" src="images_project\greentech2.jpg" alt="" />
+					  </a>
+					</div>
+					<div class="col-lg-6">
+					  <h2 class="card-title">Energy-efficient home design and construction</h2>
+					  <p class="card-text">Energy-efficient home design and construction aim to optimize energy usage and reduce waste. This involves implementing effective insulation, sealing air leaks, and utilizing passive solar design principles. By ensuring proper insulation, homeowners can reduce heat loss or gain, resulting in improved energy efficiency. Additionally, high-performance windows and energy-efficient HVAC systems contribute to lower energy consumption and enhanced comfort.</p>
+					  <a href="#" class="btn btn-primary">Read More &rarr;</a>
+					</div>
+				  </div>
+				</div>
+				<div class="card-footer text-muted">
+				  Posted on January 1, 2017 by
+				  <a href="#">GreenLifeHub101</a>
+				</div>
+			</div>
+
+			<!-- Blog Post -->
+			<div class="card mb-4">
+				<div class="card-body">
+				  <div class="row">
+					<div class="col-lg-6">
+					  <a href="#">
+						<img class="img-fluid rounded" src="images_project\greentech3.jpg" alt="" />
+					  </a>
+					</div>
+					<div class="col-lg-6">
+					  <h2 class="card-title">Renewable energy systems</h2>
+					  <p class="card-text">Renewable energy systems, such as solar panels, wind turbines, and geothermal heat pumps, offer sustainable alternatives to traditional energy sources. Solar panels convert sunlight into electricity, wind turbines harness wind power, and geothermal heat pumps utilize the earth's natural heat. By incorporating these systems into homes, homeowners can generate clean, renewable energy, reduce reliance on fossil fuels, and potentially save on electricity costs.</p>
+					  <a href="#" class="btn btn-primary">Read More &rarr;</a>
+					</div>
+				  </div>
+				</div>
+				<div class="card-footer text-muted">
+				  Posted on January 1, 2017 by
+				  <a href="#">GreenLifeHub101</a>
+				</div>
+			</div>
+
+			<!-- Blog Post -->
+			<div class="card mb-4">
+				<div class="card-body">
+				  <div class="row">
+					<div class="col-lg-6">
+					  <a href="#">
+						<img class="img-fluid rounded" src="images_project\greentech4.jpg" alt="">
+					  </a>
+					</div>
+					<div class="col-lg-6">
+					  <h2 class="card-title">Smart home technology for energy management:</h2>
+					  <p class="card-text">Smart home technology provides advanced tools for monitoring and controlling energy usage. Smart thermostats, energy monitoring systems, and home automation features enable homeowners to optimize energy consumption. With these technologies, homeowners can adjust temperature settings remotely, receive energy usage insights, and automate energy-intensive devices. This helps reduce waste, increase energy efficiency, and save on utility bills.</p>
+					  <a href="#" class="btn btn-primary">Read More &rarr;</a>
+					</div>
+				  </div>
+				</div>
+				<div class="card-footer text-muted">
+				  Posted on January 1, 2017 by
+				  <a href="#">GreenLifeHub101</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="pagination_bar_arrow">
+			<!-- Pagination -->
+			<ul class="pagination justify-content-center mb-4">
+				<li class="page-item">
+					<a class="page-link" href="#">&larr; Older</a>
+				</li>
+				<li class="page-item disabled">
+					<a class="page-link" href="#">Newer &rarr;</a>
+				</li>
+			</ul>
+		</div>
+
+    </div>
+
+  </div>
+  <!-- /.container -->
+
+	<!--footer starts from here-->
+    <footer class="footer">
+        <div class="container bottom_border">
+            <div class="row">
+               <div class="col-lg-3 col-md-6 col-sm-6 col">
+					<h5 class="headin5_amrc col_white_amrc pt2">Find us</h5>
+					<!--headin5_amrc-->
+					<p class="mb10"> We are committed to building a strong and engaged community of individuals passionate about sustainable living. Wherever you choose to connect with us, we look forward to sharing valuable information, inspiration, and support on your sustainable living journey.
+						Here are the various ways to find and connect with us:
+					</p>
+					<p><i class="fa fa-location-arrow"></i> 4905 P4 Brgy. Cabugao </p>
+					<p><i class="fa fa-phone"></i> +63-966-422-6295 or +63-997-782-1751</p>
+					<p><i class="fa fa fa-envelope"></i> kamilsaporna123@gmail.com or Angelicaserdena770@gmail.com</p>
+
+               </div>
+               <div class="col-lg-3 col-md-6 col-sm-6 col">
+					<h5 class="headin5_amrc col_white_amrc pt2">Follow us</h5>
+					<!--headin5_amrc ends here-->
+					<ul class="footer_ul2_amrc">
+						<li>
+							<a href="#"><i class="fab fa-facebook fleft padding-right"></i> </a>
+							<p>FACEBOOK <a href="#">https://www.facebook.com
+								/min.chaeeun.58</a></p>
+						</li>
+						<li>
+							<a href="#"><i class="fab fa-facebook fleft padding-right"></i> </a>
+							<p>FACEBOOK <a href="#">https://www.facebook.com
+								/acilegna.anedres</a></p>
+						</li>
+						<li>
+							<a href="#"><i class="fab fa-instagram fleft padding-right"></i> </a>
+							<p>INSTAGRAM <a href="#">https://www.instagram.com
+								/kamirusenpai/</a></p>
+						</li>
+
+
+
+
+					</ul>
+					<!--footer_ul2_amrc ends here-->
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<h5 class="headin5_amrc col_white_amrc pt2">Quick links</h5>
+					<!--headin5_amrc-->
+					<ul class="footer_ul_amrc">
+						<li><a href="#">Default Version</a></li>
+						<li><a href="#">Boxed Version</a></li>
+						<li><a href="#">Our Team </a></li>
+						<li><a href="#">About Us</a></li>
+						<li><a href="#">Our Services</a></li>
+						<li><a href="#">Get In Touch</a></li>
+					</ul>
+					<!--footer_ul_amrc ends here-->
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6 ">
+					<h5 class="headin5_amrc col_white_amrc pt2">Recent posts</h5>
+					<!--headin5_amrc-->
+					<ul class="footer_ul_amrc">
+						<li class="media">
+							<div class="media-left">
+								<img class="img-fluid" src="images_project\2HB 5.jpgg" alt="" />
+							</div>
+							<div class="media-body">
+								<p>How to find best dog food?</p>
+								<span>22 Sep 2018</span>
+							</div>
+						</li>
+						<li class="media">
+							<div class="media-left">
+								<img class="img-fluid" src="images_project\2HB6.jpg" alt="" />
+							</div>
+							<div class="media-body">
+								<p>How to find best dog food?</p>
+								<span>34 Sep 2018</span>
+							</div>
+						</li>
+						<li class="media">
+							<div class="media-left">
+								<img class="img-fluid" src="images/post-img-03.jpg" alt="" />
+							</div>
+							<div class="media-body">
+								<p>How to find best dog food?</p>
+								<span>30 Sep 2018</span>
+							</div>
+						</li>
+					</ul>
+					<!--footer_ul_amrc ends here-->
+				</div>
+			</div>
+		</div>
+        <div class="container">
+            <div class="footer-logo">
+				<a href="#"><img src="images_project\LOGOFINAL.png" alt="" /></a>
+			</div>
+            <!--foote_bottom_ul_amrc ends here-->
+            <p class="copyright text-center">All Rights Reserved. &copy; 2018 <a href="#">GreenLifeHub101</a> Design By : 
+				<a href="https://html.design/">html design</a>
+            </p>
+            <ul class="social_footer_ul">
+				<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+				<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+				<li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+				<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+            <!--social_footer_ul ends here-->
+        </div>
+    </footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
