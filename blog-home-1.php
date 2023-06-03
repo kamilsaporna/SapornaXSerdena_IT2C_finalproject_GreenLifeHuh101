0252ff31558736b59dfab17d7bafdf502d93fe10
AngelicaSerdena <angelicaserdena770@gmail.com>
2023-06-03 23:48:14 +0800

    Third commit

---
+++ blog-home-1.php
@@ -0,0 +1,382 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>GreenLifeHub101 </title>
+	<!-- Bootstrap core CSS -->
+	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Fontawesome CSS -->
+	<link href="css/all.css" rel="stylesheet">
+	<!-- Custom styles for this template -->
+	<link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+	<!-- Navigation -->
+
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-light top-nav fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+            <img src="images_project\LOGOFINAL.png" alt="logo" />
+            </a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="fas fa-bars"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+               <ul class="navbar-nav ml-auto">
+                  <li class="nav-item">
+                     <a class="nav-link" href="index.php">Home</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="about.php">About</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="services.php">Services</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                     Portfolio
+                     </a>
+					 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+                        <a class="dropdown-item" href="portfolio-1-col.php">Green Living Tips</a>
+                        <a class="dropdown-item" href="portfolio-2-col.php">Gardening Urban Farming</a>
+                        <a class="dropdown-item" href="portfolio-3-col.php">DIY Projects</a>
+                        <a class="dropdown-item" href="portfolio-4-col.php">Community and Engagement</a>
+                        <a class="dropdown-item" href="portfolio-item.php">Environmental News Education</a>
+                     </div>
+                  </li>
+				  <li class="nav-item dropdown">
+					<a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Blog
+					</a>
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+					   <a class="dropdown-item" href="blog-home-1.php">Sustainable Home</a>
+					   <a class="dropdown-item" href="blog-home-2.php">Green Technology</a>
+					   <a class="dropdown-item" href="blog-post.php">Eco-Friendly Gardening</a>
+					</div>
+				 </li>
+                  <li class="nav-item dropdown">
+                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                     Pages
+                     </a>
+                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+                        <a class="dropdown-item" href="faq.php">FAQ</a>
+                        <a class="dropdown-item" href="404.php">404</a>
+                        <a class="dropdown-item" href="pricing.php">Pricing Table</a>
+                     </div>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="contact.php">Contact</a>
+                  </li>
+               </ul>
+            </div>
+        </div>
+    </nav>
+	
+	<!-- full Title -->
+	<div class="full-title">
+		<div class="container">
+			<!-- Page Heading/Breadcrumbs -->
+			<h1 class="mt-4 mb-3">Sustainable Home
+				<small>Welcome to our Sustainable Home section, where we empower you to create an eco-friendly and sustainable living space. Here, we share tips, ideas, and resources to help you reduce your environmental impact, conserve resources, and create a healthier home for both you and the planet.</small></h1>
+			
+			</h1>
+		</div>
+	</div>
+
+    <!-- Page Content -->
+    <div class="container">
+		<div class="breadcrumb-main">
+			<ol class="breadcrumb">
+				<li class="breadcrumb-item">
+					<a href="index.php">Home</a>
+				</li>
+				<li class="breadcrumb-item active">Sustainable Home</li>
+			</ol>
+		</div>
+
+		<div class="row">
+			<!-- Blog Entries Column -->
+			<div class="col-md-8 blog-entries">
+			  <!-- Blog Post -->
+			  <div class="card mb-4">
+				<img class="card-img-top" src="images_project\energyefficiency.jpg" alt="Card image Blog" />
+				<div class="card-body">
+				  <h2 class="card-title">Energy Efficiency:</h2>
+				  <p class="card-text">Tips for conducting a home energy audit to identify areas of improvement:
+
+					A home energy audit is a valuable tool for identifying energy-wasting areas in your home. Start by conducting a thorough inspection of your house, checking for air leaks around windows and doors, inspecting insulation in walls and attic, and assessing the energy consumption of appliances and electronics.
+					Look for gaps or cracks in windows and doors and seal them with weatherstripping or caulk to prevent air leakage. Inspect insulation and add more if needed to improve energy efficiency.
+					Use a power meter or energy monitor to measure the energy usage of appliances and electronics. Identify energy-hungry devices and consider upgrading to more energy-efficient models.</p>
+				  <a href="#" class="btn btn-primary">Read More &rarr;</a>
+
+				  <p class="card-text">Explanation of energy-efficient lighting options, such as LED bulbs:
+					LED (Light Emitting Diode) bulbs are a highly energy-efficient lighting option. They consume significantly less energy compared to traditional incandescent bulbs and have a longer lifespan.
+					LED bulbs also produce less heat, making them safer to use. They are available in various color temperatures and can provide the desired lighting ambiance for different rooms in your home.
+					When choosing LED bulbs, consider factors such as wattage, color temperature (measured in Kelvin), and lumens (brightness). Look for Energy Star certified bulbs for guaranteed quality and energy efficiency.</p>
+				  <a href="#" class="btn btn-primary">Read More &rarr;</a>
+
+				  <p class="card-text">Information on the benefits of programmable thermostats and smart home energy management systems:
+
+					Programmable thermostats allow you to set different temperature schedules for different times of the day, ensuring that heating or cooling is optimized for when you're at home or away. This helps save energy and reduce utility bills.
+Smart thermostats take energy efficiency to the next level by providing additional features like remote access via smartphone apps, learning capabilities that adjust settings based on your preferences and behavior, and integration with other smart home devices for enhanced automation and energy management.</p>
+				  <a href="#" class="btn btn-primary">Read More &rarr;</a>
+
+				  <p class="card-text">Guidance on how to properly insulate your home to reduce energy loss:
+
+					Proper insulation is crucial for preventing heat transfer and reducing energy loss in your home. Insulate walls, attic, floors, and crawl spaces to create a thermal barrier.
+					Common insulation materials include fiberglass, cellulose, spray foam, and rigid foam. Each material has its own benefits and considerations, so choose the one that suits your specific needs and budget.
+					Ensure that insulation is installed correctly, without gaps or compression, to maximize its effectiveness. Seek professional assistance if needed, especially for complex insulation projects.</p>
+				  <a href="#" class="btn btn-primary">Read More &rarr;</a>
+
+				  <p class="card-text">Overview of renewable energy options, including solar panels and wind turbines:
+
+Renewable energy sources like solar panels and wind turbines offer sustainable alternatives to traditional energy sources. Solar panels convert sunlight into electricity, while wind turbines harness wind energy.
+Installing solar panels on your roof can significantly reduce your reliance on grid electricity and even allow you to sell excess power back to the grid. Wind turbines, if suitable for your location, can also generate clean energy.
+Consider factors such as available sunlight or wind resources, local regulations and incentives, and your budget before deciding to invest in renewable energy systems. Consult with experts to determine the feasibility and potential benefits.</p>
+				  <a href="#" class="btn btn-primary">Read More &rarr;</a>
+				  
+				</div>
+				<div class="card-footer text-muted">
+				  Posted on January 1, 2018 by
+				  <a href="#">GreenLifeHub101</a>
+				</div>
+			  </div>
+
+			  <!-- Blog Post -->
+			  <div class="card mb-4">
+				<img class="card-img-top" src="images_project\waterconservation.jpg" alt="Card image Blog">
+				<div class="card-body">
+				  <h2 class="card-title">Water Conservation</h2>
+				  <p class="card-text">Tips for reducing water usage in the home:
+
+					Be mindful of water consumption by adopting simple habits like turning off the faucet while brushing teeth, taking shorter showers, and fixing leaks promptly.
+					Use water-efficient appliances and fixtures, such as low-flow showerheads, faucets, and toilets, to reduce water usage without compromising functionality.
+					Water outdoor plants and gardens during the early morning or late evening to minimize evaporation.
+					Consider installing a water meter to track usage and identify areas for improvement.</p>
+
+					<p class="card-text">Tips for landscaping with water-efficient practices:
+
+						Encourage readers to choose drought-tolerant plants that require less water and are well-suited to the local climate.
+						Discuss the benefits of xeriscaping, a landscaping technique that focuses on water conservation by using native plants, mulch, and efficient irrigation systems.
+						Provide guidance on proper irrigation practices, such as using drip irrigation systems, adjusting watering schedules based on weather conditions, and avoiding overwatering.nt.</p>
+		
+				  <a href="#" class="btn btn-primary">Read More &rarr;</a>
+				</div>
+				<div class="card-footer text-muted">
+				  Posted on January 1, 2017 by
+				  <a href="#">GreenLifeHub101</a>
+				</div>
+			  </div>
+
+			  <!-- Blog Post -->
+			  <div class="card mb-4">
+				<img class="card-img-top" src="images_project\waterreduction.jpg" alt="Card image Blog">
+				<div class="card-body">
+				  <h2 class="card-title">Water Reduction</h2>
+				  <p class="card-text">Tips for reducing water usage in the home:
+
+					Be mindful of water consumption by adopting simple habits like turning off the faucet while brushing teeth, taking shorter showers, and fixing leaks promptly.
+					Use water-efficient appliances and fixtures, such as low-flow showerheads, faucets, and toilets, to reduce water usage without compromising functionality.
+					Water outdoor plants and gardens during the early morning or late evening to minimize evaporation.</p>
+				  <a href="#" class="btn btn-primary">Read More &rarr;</a>
+				</div>
+				<div class="card-footer text-muted">
+				  Posted on January 1, 2017 by
+				  <a href="#">GreenLifeHub101</a>
+				</div>
+			  </div>
+
+				<div class="pagination_bar_arrow">
+				  <!-- Pagination -->
+					<ul class="pagination justify-content-center mb-4">
+						<li class="page-item">
+							<a class="page-link" href="#">&larr; Older</a>
+						</li>
+						<li class="page-item disabled">
+							<a class="page-link" href="#">Newer &rarr;</a>
+						</li>
+					</ul>
+				</div>
+
+			</div>
+
+			<!-- Sidebar Widgets Column -->
+			<div class="col-md-4 blog-right-side">
+
+			  <!-- Search Widget -->
+			  <div class="card mb-4">
+				<h5 class="card-header">Search</h5>
+				<div class="card-body">
+				  <div class="input-group">
+					<input type="text" class="form-control" placeholder="Search for...">
+					<span class="input-group-btn">
+					  <button class="btn btn-secondary" type="button">Go!</button>
+					</span>
+				  </div>
+				</div>
+			  </div>
+
+			  <!-- Categories Widget -->
+			  <div class="card my-4">
+				<h5 class="card-header">Categories</h5>
+				<div class="card-body">
+				  <div class="row">
+					<div class="col-lg-6">
+					  <ul class="list-unstyled mb-0">
+						<li>
+						  <a href="#">Energy Efficiency</a>
+						</li>
+						<li>
+						  <a href="#">Water Conservation</a>
+						</li>
+						<li>
+						  <a href="#">Waste Reduction</a>
+						</li>
+					  </ul>
+					</div>
+					<div class="col-lg-6">
+					  <ul class="list-unstyled mb-0">
+						<li>
+						  <a href="#">Sustainable Home Decor</a>
+						</li>
+						<li>
+						  <a href="#">DIY and Upcycling</a>
+						</li>
+						<li>
+						  <a href="#">Indoor Air Quality</a>
+						</li>
+					  </ul>
+					</div>
+				  </div>
+				</div>
+			  </div>
+
+			  <!-- Side Widget -->
+			  <div class="card my-4">
+				<h5 class="card-header">Message</h5>
+				<div class="card-body">
+					Discover a wealth of information and practical advice on sustainable home practices, energy-saving strategies, waste reduction, and eco-friendly home decor. Whether you're a homeowner, renter, or simply looking to make positive changes in your living space, our aim is to provide inspiration and actionable steps for a greener home.
+			  </div>
+
+			</div>
+
+		</div>
+		<!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+    <!--footer starts from here-->
+    <footer class="footer">
+        <div class="container bottom_border">
+            <div class="row">
+               <div class="col-lg-3 col-md-6 col-sm-6 col">
+					<h5 class="headin5_amrc col_white_amrc pt2">Find us</h5>
+					<!--headin5_amrc-->
+					<p class="mb10"> We are committed to building a strong and engaged community of individuals passionate about sustainable living. Wherever you choose to connect with us, we look forward to sharing valuable information, inspiration, and support on your sustainable living journey.
+						Here are the various ways to find and connect with us:
+					</p>
+					<p><i class="fa fa-location-arrow"></i> 4905 P4 Brgy. Cabugao </p>
+					<p><i class="fa fa-phone"></i> +63-966-422-6295 or +63-997-782-1751</p>
+					<p><i class="fa fa fa-envelope"></i> kamilsaporna123@gmail.com or Angelicaserdena770@gmail.com</p>
+
+               </div>
+               <div class="col-lg-3 col-md-6 col-sm-6 col">
+					<h5 class="headin5_amrc col_white_amrc pt2">Follow us</h5>
+					<!--headin5_amrc ends here-->
+					<ul class="footer_ul2_amrc">
+						<li>
+							<a href="#"><i class="fab fa-facebook fleft padding-right"></i> </a>
+							<p>FACEBOOK <a href="#">https://www.facebook.com
+								/min.chaeeun.58</a></p>
+						</li>
+						<li>
+							<a href="#"><i class="fab fa-facebook fleft padding-right"></i> </a>
+							<p>FACEBOOK <a href="#">https://www.facebook.com
+								/acilegna.anedres</a></p>
+						</li>
+						<li>
+							<a href="#"><i class="fab fa-instagram fleft padding-right"></i> </a>
+							<p>INSTAGRAM <a href="#">https://www.instagram.com
+								/kamirusenpai/</a></p>
+						</li>
+
+
+					</ul>
+					<!--footer_ul2_amrc ends here-->
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<h5 class="headin5_amrc col_white_amrc pt2">Quick links</h5>
+					<!--headin5_amrc-->
+					<ul class="footer_ul_amrc">
+						<li><a href="#">Default Version</a></li>
+						<li><a href="#">Boxed Version</a></li>
+						<li><a href="#">Our Team </a></li>
+						<li><a href="#">About Us</a></li>
+						<li><a href="#">Our Services</a></li>
+						<li><a href="#">Get In Touch</a></li>
+					</ul>
+					<!--footer_ul_amrc ends here-->
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6 ">
+					<h5 class="headin5_amrc col_white_amrc pt2">Recent posts</h5>
+					<!--headin5_amrc-->
+					<ul class="footer_ul_amrc">
+						<li class="media">
+							<div class="media-left">
+								<img class="img-fluid" ="images/post-img-01.jpg" alt="" />
+							</div>
+							<div class="media-body">
+								<p>How to find best dog food?</p>
+								<span>22 Sep 2018</span>
+							</div>
+						</li>
+						<li class="media">
+							<div class="media-left">
+								<img class="img-fluid" src="images_project\HB4.jpg" alt="" />
+							</div>
+							<div class="media-body">
+								<p>How to find best dog food?</p>
+								<span>34 Sep 2018</span>
+							</div>
+						</li>
+						<li class="media">
+							<div class="media-left">
+								<img class="img-fluid" src="images_project\HB5.jpg" alt="" />
+							</div>
+							<div class="media-body">
+								<p>How to find best dog food?</p>
+								<span>30 Sep 2018</span>
+							</div>
+						</li>
+					</ul>
+					<!--footer_ul_amrc ends here-->
+				</div>
+			</div>
+		</div>
+        <div class="container">
+            <div class="footer-logo">
+				<a href="#"><img src="images_project\HB6.jpg" alt="" /></a>
+			</div>
+            <!--foote_bottom_ul_amrc ends here-->
+            <p class="copyright text-center">All Rights Reserved. &copy; 2018 <a href="#">GreenLifeHub101</a> Design By : 
+				<a href="https://html.design/">html design</a>
+            </p>
+            <ul class="social_footer_ul">
+				<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+				<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+				<li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+				<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+            <!--social_footer_ul ends here-->
+        </div>
+    </footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
