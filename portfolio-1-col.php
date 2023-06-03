4a3165d9a66e048dad0c1f3a8a29a206eedf3cd6
AngelicaSerdena <angelicaserdena770@gmail.com>
2023-06-03 23:55:02 +0800

    Tenth commit

---
+++ portfolio-1-col.php
@@ -0,0 +1,333 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>GreenLifeHub101 </title>
+	<!-- Bootstrap core CSS -->
+	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Fontawesome CSS -->
+	<link href="css/all.css" rel="stylesheet">
+	<!-- Custom styles for this template -->
+	<link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Navigation -->
+	
+
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-light top-nav fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+            <img src="images_project\LOGOFINAL.png" alt="logo" />
+            </a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="fas fa-bars"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+               <ul class="navbar-nav ml-auto">
+                  <li class="nav-item">
+                     <a class="nav-link" href="index.php">Home</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="about.php">About</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="services.php">Services</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                     <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                     Portfolio
+                     </a>
+					 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+                        <a class="dropdown-item" href="portfolio-1-col.php">Green Living Tips</a>
+                        <a class="dropdown-item" href="portfolio-2-col.php">Gardening Urban Farming</a>
+                        <a class="dropdown-item" href="portfolio-3-col.php">DIY Projects</a>
+                        <a class="dropdown-item" href="portfolio-4-col.php">Community and Engagement</a>
+                        <a class="dropdown-item" href="portfolio-item.phpl">Environmental News Education</a>
+                     </div>
+                  </li>
+				  <li class="nav-item dropdown">
+					<a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Blog
+					</a>
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+					   <a class="dropdown-item" href="blog-home-1.php">Sustainable Home</a>
+					   <a class="dropdown-item" href="blog-home-2.php">Green Technology</a>
+					   <a class="dropdown-item" href="blog-post.php">Eco-Friendly Gardening</a>
+					</div>
+				 </li>
+                  <li class="nav-item dropdown">
+                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                     Pages
+                     </a>
+                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+                        <a class="dropdown-item" href="faq.php">FAQ</a>
+                        <a class="dropdown-item" href="404.php">404</a>
+                        <a class="dropdown-item" href="pricing.php">Pricing Table</a>
+                     </div>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="contact.php">Contact</a>
+                  </li>
+               </ul>
+            </div>
+        </div>
+    </nav>
+	
+	<!-- full Title -->
+	<div class="full-title">
+		<div class="container">
+			<!-- Page Heading/Breadcrumbs -->
+			<h1 class="mt-4 mb-3">Green Living Tips
+				<small>Empower Change, Live Sustainably: Discover Green Living Tips for a Greener Future</small>
+			</h1>
+		</div>
+	</div>
+
+    <!-- Page Content -->
+    <div class="container">
+		<div class="breadcrumb-main">
+			<ol class="breadcrumb">
+				<li class="breadcrumb-item">
+					<a href="index.php">Home</a>
+				</li>
+				<li class="breadcrumb-item active">Green Living Tips</li>
+			</ol>
+		</div>
+	<div class="project-inner">
+		<!-- Project One -->
+		<div class="row">
+			<div class="col-md-7">
+				<a href="#">
+					<img class="img-fluid rounded mb-3 mb-md-0" src="images_project\treeAndHands.jpg" alt="" />
+				</a>
+			</div>
+			<div class="col-md-5">
+				<h3>Conserve Energy</h3>
+				<p>Turn off lights and appliances when not in use, switch to energy-efficient LED bulbs, and consider installing a programmable thermostat to optimize energy usage.</p>
+				<h3>Reduce Water Consumption</h3>
+				<p> Fix leaks, install water-efficient fixtures, collect rainwater for gardening, and practice mindful water usage, such as shorter showers and turning off the tap while brushing teeth.</p>
+				<h3>Embrace Recycling</h3>
+				<p> Set up a recycling system at home and educate yourself on what can be recycled in your area. Opt for reusable products like water bottles, shopping bags, and cloth napkins instead of single-use items.</p>
+				
+				<a class="btn btn-primary" href="#">View Project
+					<span class="glyphicon glyphicon-chevron-right"></span>
+				</a>
+			</div>
+		</div>
+		<!-- /.row -->
+	</div>
+    <hr>
+	<div class="project-inner">
+		<!-- Project Two -->
+		<div class="row">
+			<div class="col-md-7">
+			  <a href="#">
+				<img class="img-fluid rounded mb-3 mb-md-0" src="images_project\showeringplants.jpg" alt="" />
+			  </a>
+			</div>
+			<div class="col-md-5">
+				<h3>Minimize Waste</h3>
+				<p> Reduce waste by composting food scraps and yard waste, opting for products with minimal packaging, and reusing or repurposing items whenever possible.</p>
+				<h3>Choose Sustainable Transportation</h3>
+				<p> Walk, bike, or use public transportation whenever feasible. If you need a car, consider carpooling, using electric or hybrid vehicles, or exploring car-sharing services.</p>
+				<h3>Educate and Inspire</h3>
+				<p> Share your knowledge and passion for sustainable living with others. Engage in conversations, organize workshops or events, and use social media platforms to spread awareness and inspire positive change.</p>
+				<h3>Connect with Nature</h3>
+				<p> Spend time outdoors, connect with nature, and develop an appreciation for the environment. Consider supporting local conservation efforts and volunteering for environmental organizations.</p>
+			  <a class="btn btn-primary" href="#">View Project
+				<span class="glyphicon glyphicon-chevron-right"></span>
+			  </a>
+			</div>
+		</div>
+		<!-- /.row -->
+	</div>
+    <hr>
+	<div class="project-inner">
+		<!-- Project Three -->
+		<div class="row">
+			<div class="col-md-7">
+				<a href="#">
+					<img class="img-fluid rounded mb-3 mb-md-0" src="images_project\bulb_image.jpg" alt="" />
+				</a>
+			</div>
+			<div class="col-md-5">
+				<h3>Embrace a Plant-Based Diet</h3>
+				<p> Reduce your carbon footprint by incorporating more plant-based meals into your diet. This can help conserve resources, reduce greenhouse gas emissions, and promote a healthier lifestyle.</p>
+				<h3>Practice Mindful Consumption</h3>
+				<p> Before making a purchase, consider the environmental impact of the product, its durability, and whether you truly need it. Opt for second-hand or eco-friendly alternatives whenever possible.</p>
+				<h3>Support Local and Organic Food</h3>
+				<p> Buy locally grown, seasonal produce to reduce the carbon footprint associated with long-distance transportation. Choose organic and sustainably produced foods to support healthier ecosystems and minimize pesticide use.</p>
+					<span class="glyphicon glyphicon-chevron-right"></span>
+				</a>
+			</div>
+		</div>
+		<!-- /.row -->
+	</div>
+    <hr>
+
+    <!-- Project Four -->
+	<div class="project-inner">
+		<div class="row">		
+			<div class="col-md-7">
+				<a href="#">
+					<img class="img-fluid rounded mb-3 mb-md-0" src="images_project\news.jpg" alt="" />
+				</a>
+			</div>
+			<div class="col-md-5">
+				<h3>Choose Natural Cleaning Products</h3>
+				<p>Use eco-friendly and non-toxic cleaning products to reduce exposure to harmful chemicals and minimize environmental impact. You can also make your own cleaning solutions using simple, natural ingredients like vinegar and baking soda.</p>
+				<h3>Reduce Plastic Waste</h3>
+				<p>Say no to single-use plastics by carrying reusable shopping bags, water bottles, and coffee cups. Choose products with minimal packaging and opt for sustainable alternatives like bamboo toothbrushes and stainless steel straws.</p>
+				<a class="btn btn-primary" href="#">View Project
+					<span class="glyphicon glyphicon-chevron-right"></span>
+				</a>
+			</div>
+		</div>
+		<!-- /.row -->
+	</div>
+	
+		<hr>
+
+		<div class="pagination_bar">
+			<!-- Pagination -->
+			<ul class="pagination justify-content-center">
+				<li class="page-item">
+				  <a class="page-link" href="#" aria-label="Previous">
+					<span aria-hidden="true">&laquo;</span>
+					<span class="sr-only">Previous</span>
+				  </a>
+				</li>
+				<li class="page-item">
+				  <a class="page-link" href="#">1</a>
+				</li>
+				<li class="page-item">
+				  <a class="page-link" href="#">2</a>
+				</li>
+				<li class="page-item">
+				  <a class="page-link" href="#">3</a>
+				</li>
+				<li class="page-item">
+				  <a class="page-link" href="#" aria-label="Next">
+					<span aria-hidden="true">&raquo;</span>
+					<span class="sr-only">Next</span>
+				  </a>
+				</li>
+			</ul>
+		</div>
+    </div>
+    <!-- /.container -->
+
+    <!--footer starts from here-->
+    <footer class="footer">
+        <div class="container bottom_border">
+            <div class="row">
+               <div class="col-lg-3 col-md-6 col-sm-6 col">
+					<h5 class="headin5_amrc col_white_amrc pt2">Find us</h5>
+					<!--headin5_amrc-->
+					<p class="mb10"> We are committed to building a strong and engaged community of individuals passionate about sustainable living. Wherever you choose to connect with us, we look forward to sharing valuable information, inspiration, and support on your sustainable living journey.
+						Here are the various ways to find and connect with us:
+					</p>
+					<p><i class="fa fa-location-arrow"></i> 4905 P4 Brgy. Cabugao </p>
+					<p><i class="fa fa-phone"></i> +63-966-422-6295 or +63-997-782-1751</p>
+					<p><i class="fa fa fa-envelope"></i> kamilsaporna123@gmail.com or Angelicaserdena770@gmail.com</p>
+
+               </div>
+               <div class="col-lg-3 col-md-6 col-sm-6 col">
+					<h5 class="headin5_amrc col_white_amrc pt2">Follow us</h5>
+					<!--headin5_amrc ends here-->
+					<ul class="footer_ul2_amrc">
+						<li>
+							<a href="#"><i class="fab fa-facebook fleft padding-right"></i> </a>
+							<p>FACEBOOK <a href="#">https://www.facebook.com
+								/min.chaeeun.58</a></p>
+						</li>
+						<li>
+							<a href="#"><i class="fab fa-facebook fleft padding-right"></i> </a>
+							<p>FACEBOOK <a href="#">https://www.facebook.com
+								/acilegna.anedres</a></p>
+						</li>
+						<li>
+							<a href="#"><i class="fab fa-instagram fleft padding-right"></i> </a>
+							<p>INSTAGRAM <a href="#">https://www.instagram.com
+								/kamirusenpai/</a></p>
+						</li>
+
+					</ul>
+					<!--footer_ul2_amrc ends here-->
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<h5 class="headin5_amrc col_white_amrc pt2">Quick links</h5>
+					<!--headin5_amrc-->
+					<ul class="footer_ul_amrc">
+						<li><a href="#">Default Version</a></li>
+						<li><a href="#">Boxed Version</a></li>
+						<li><a href="#">Our Team </a></li>
+						<li><a href="#">About Us</a></li>
+						<li><a href="#">Our Services</a></li>
+						<li><a href="#">Get In Touch</a></li>
+					</ul>
+					<!--footer_ul_amrc ends here-->
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6 ">
+					<h5 class="headin5_amrc col_white_amrc pt2">Recent posts</h5>
+					<!--headin5_amrc-->
+					<ul class="footer_ul_amrc">
+						<li class="media">
+							<div class="media-left">
+								<img class="img-fluid" src="images_project\PLANT IN METAL.jpg" alt="" />
+							</div>
+							<div class="media-body">
+								<p>How to find best dog food?</p>
+								<span>22 Sep 2018</span>
+							</div>
+						</li>
+						<li class="media">
+							<div class="media-left">
+								<img class="img-fluid" src="images_project\PLANT IN HANDS.jpg" alt="" />
+							</div>
+							<div class="media-body">
+								<p>How to find best dog food?</p>
+								<span>34 Sep 2018</span>
+							</div>
+						</li>
+						<li class="media">
+							<div class="media-left">
+								<img class="img-fluid" src="images_project\PLANT.jpg" alt="" />
+							</div>
+							<div class="media-body">
+								<p>How to find best dog food?</p>
+								<span>30 Sep 2018</span>
+							</div>
+						</li>
+					</ul>
+					<!--footer_ul_amrc ends here-->
+				</div>
+			</div>
+		</div>
+        <div class="container">
+            <div class="footer-logo">
+				<a href="#"><img src="images_project\LOGOFINAL.png" alt="" /></a>
+			</div>
+            <!--foote_bottom_ul_amrc ends here-->
+            <p class="copyright text-center">All Rights Reserved. &copy; 2018 <a href="#">GreenLifeHub101</a> Design By : 
+				<a href="https://html.design/">html design</a>
+            </p>
+            <ul class="social_footer_ul">
+				<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+				<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+				<li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+				<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+            <!--social_footer_ul ends here-->
+        </div>
+    </footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
